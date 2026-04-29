2cd20dab278f0b18edc7b4d5e44d7ee7fc3a6d41
Kripesh <dhungelkripesh@gmail.com>
2026-04-29 11:18:23 -0400

    ...

--- logic.py
+++ logic.py
@@ -12,7 +12,10 @@ def calculate_wealth(monthly_investment, annual_rate, years):
     for m in range(1, months + 1):
 
         current_balance = (current_balance + monthly_investment) * (1 + monthly_rate)
+        if m % 12 == 0:
 
-        
+            balances.append(round(current_balance, 2))
+            labels.append(f"Year {m//12}")
+
 
 
