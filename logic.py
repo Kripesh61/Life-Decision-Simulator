40e2bdfe329f3753520c83de3b2aa13fd7376d08
Kripesh <dhungelkripesh@gmail.com>
2026-04-29 15:10:27 +0000

    Apr 29, 2026, 11:10 AM

--- logic.py
+++ logic.py
@@ -5,4 +5,8 @@ def calculate_wealth(monthly_investment, annual_rate, years):
     months = years * 12
     monthly_rate = (annual_rate / 100) / 12
 
+    balances = []
+    labels = []
+    current_balance = 0
+
     
