783913a5d7f29e3742047bf2b4fa5dc11c123598
Kripesh <dhungelkripesh@gmail.com>
2026-05-10 01:41:00 +0000

    May 9, 2026, 9:41 PM

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.title("Life Decision Simulator")
 
 # --- SIDEBAR INPUTS ---
 st.sidebar.header("Scenario Settings")
-
+#Life-Decision-Simulator
 name = st.sidebar.text_input("Scenario Name", "New Car vs Savings")
 monthly = st.sidebar.number_input("Monthly Contribution ($)", 0, 10000, 500)
 years = st.sidebar.slider("Time Horizon (Years)", 1, 40, 10)
