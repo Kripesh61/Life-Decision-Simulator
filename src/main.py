03c08dda708aa55bc47950840a31b57640cf37ad
Kripesh <dhungelkripesh@gmail.com>
2026-05-10 21:29:15 -0400

    bb

--- src/main.py
+++ src/main.py
@@ -62,7 +62,7 @@ if st.button("Save This Scenario"):
 # Load Table
 if os.path.exists(get_data_path("scenarios.json")):
     with open(get_data_path("scenarios.json"), "r") as f:
-        st.table(pd.DataFrame(json.load(f)))
+        
 
 
 
